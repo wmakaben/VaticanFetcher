3305c0567244a8c00468979464f23db1a896e7e2
William Makabenta <will06hm@gmail.com>
2013-12-10 12:15:32 +0100

    just a mess of things to change

--- src/net/sourceforge/docfetcher/build/BuildMain.java
+++ src/net/sourceforge/docfetcher/build/BuildMain.java
@@ -175,7 +175,7 @@ public final class BuildMain {
 		makeExecutable("Cannot make the portable launcher shell scripts executable.", linuxLauncher, macOsXLauncher);
 
 		String exeLauncher = U.format("%s/%s.exe", releaseDir, appName);
-		U.copyBinaryFile("dist/launchers/DocFetcher-512.exe", exeLauncher);
+		U.copyBinaryFile("dist/launchers/VaticanFetcher-512.exe", exeLauncher);
 
 		for (int heapSize : new int[] {256, 512, 768, 1024}) {
 			String exeName = U.format("%s-%d.exe", appName, heapSize);
